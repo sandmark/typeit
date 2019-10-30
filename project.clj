e317007a3d38e0acc0acc55e168a7b49d8b6a613
sandmark <sandmark.m@gmail.com>
2019-10-31 08:18:45 +0900

    Install Figwheel.main

--- project.clj
+++ project.clj
@@ -2,12 +2,19 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
+
   :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.520"]
                  [duct/core "0.7.0"]
                  [duct/module.logging "0.4.0"]]
+
   :plugins [[duct/lein-duct "0.12.1"]]
+
   :main ^:skip-aot typeit.main
-  :resource-paths ["resources" "target/resources"]
+
+  :resource-paths ["resources" "target" "target/resources"]
+  :clean-targets ^{:protect false} ["target/public"]
+
   :prep-tasks     ["javac" "compile" ["run" ":duct/compiler"]]
   :middleware     [lein-duct.plugin/middleware]
   :profiles
@@ -19,4 +26,9 @@
    :project/dev  {:source-paths   ["dev/src"]
                   :resource-paths ["dev/resources"]
                   :dependencies   [[integrant/repl "0.3.1"]
-                                   [eftest "0.5.9"]]}})
+                                   [eftest "0.5.9"]
+                                   [com.bhauman/figwheel-main "0.2.3"]
+                                   [com.bhauman/rebel-readline-cljs "0.1.4"]]}}
+
+  :aliases {"fig"       ["trampoline" "run" "-m" "figwheel.main"]
+            "fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]})
---
+++ src/typeit/cljs/core.cljs
@@ -0,0 +1,4 @@
+(ns typeit.cljs.core)
+
+(enable-console-print!)
+(js/console.log "Hello, World!")
